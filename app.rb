fed79b07611b8b6c6b594e8866a6664f7ef41005
Lan Pham <lanpham312@gmail.com>
2017-12-06 12:35:52 +0000

    Entering Players

--- app.rb
+++ app.rb
@@ -3,7 +3,13 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Testing infrastructure working!'
+    erb(:index)
+  end
+
+  post '/names' do
+    @first_player_name = params["player_1"]
+    @second_player_name = params["player_2"]
+    erb(:index)
   end
 
   # start the server if ruby file executed directly
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,14 @@
 feature "Testing Infrastructure" do
-  scenario "testing homepage displaying information" do
+  # scenario "testing homepage displaying information" do
+  #   visit('/')
+  #   expect(page).to have_content "Testing infrastructure working!"
+  # end
+
+  scenario "letting players fill in their names in a form and submit" do
     visit('/')
-    expect(page).to have_content "Testing infrastructure working!"
+    fill_in("player_1", with: "James")
+    fill_in("player_2", with: "Lan")
+    click_on("Submit")
+    expect(page).to have_content "James Lan"
   end
 end
